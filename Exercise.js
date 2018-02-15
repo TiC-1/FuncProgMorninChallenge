e534d8436c3e0df2a6a501c20fe63516c9d6f235
Giulia Spina <giulia.spina@gmail.com>
2018-02-15 16:10:42 +0100

    last exercise file

--- Exercise.js
+++ Exercise.js
@@ -8,10 +8,11 @@
 //1. Loop over an array of names with a for loop and push each name into a new array,
 //then return it.
 function newNames(arrayOfNames) {
-  var newArray=[], counter;
-	for(counter = 0; counter < arrayOfNames.length; counter++) {
-		newArray.push("new "+ arrayOfNames[counter]);
-	}
+  var newArray = [],
+    counter;
+  for (counter = 0; counter < arrayOfNames.length; counter++) {
+    newArray.push("new " + arrayOfNames[counter]);
+  }
   return newArray;
 }
 
@@ -19,10 +20,10 @@ function newNames(arrayOfNames) {
 //Notice that forEach lets us specify what we want to happen to each item
 //in the array, but hides how the array is traversed.
 function newNames2(arrayOfNames) {
-  var newArray=[];
-	arrayOfNames.forEach(function(name) {
-    newArray.push("new "+ name);
-	});
+  var newArray = [];
+  arrayOfNames.forEach(function(name) {
+    newArray.push("new " + name);
+  });
   return newArray;
 }
 
@@ -31,65 +32,87 @@ function newNames2(arrayOfNames) {
 function objectProjection(arrayOfObjects) {
   var videoAndTitlePairs = [];
 
-	// ------------ INSERT CODE HERE! -----------------------------------
+  // ------------ INSERT CODE HERE! -----------------------------------
   arrayOfObjects.forEach(function(film) {
-    videoAndTitlePairs.push({ id: film.id , title: film.title });
+    videoAndTitlePairs.push({
+      id: film.id,
+      title: film.title
+    });
   });
-	return videoAndTitlePairs;
+  return videoAndTitlePairs;
 }
 
 //4. Implement myMap
 Array.prototype.myMap = function(projectionFunction) {
-	var results = [];
-	this.forEach(function(itemInArray) {
+  var results = [];
+  this.forEach(function(itemInArray) {
 
-		// ------------ INSERT CODE HERE! ----------------------------
-		// Apply the projectionFunction to each item in the array and add
-		// each result to the results array.
-    results.push(itemInArray + 1);
-	});
-	return results;
+    // ------------ INSERT CODE HERE! ----------------------------
+    // Apply the projectionFunction to each item in the array and add
+    // each result to the results array.
+    results.push(projectionFunction(itemInArray));
+  });
+  return results;
 };
 
 //5. Use myMap() to project an array of videos into an array of {id,title} pairs
 function objectProjection2(arrayOfObjects) {
-  return arrayOfObjects.myMap// finish this expression!
+  var cucu = function(item) {
+    return ({
+      id: item.id,
+      title: item.title
+    });
+  }
+  return arrayOfObjects.myMap(cucu); // finish this expression!
 }
 
 //Like projection, filtering an array is also a very common operation.
 //To filter an array we apply a test to each item in the array and collect the items that pass into a new array.
 
 //6. Use forEach() to collect only those videos with a rating of 5.0
-function goodMovie(arrayOfObjects){
+function goodMovie(arrayOfObjects) {
   videos = [];
+  arrayOfObjects.forEach(function(film) {
+    if (film.rating == 5) {
+      videos.push(film);
+    };
+  })
+  // ------------ INSERT CODE HERE! -----------------------------------
 
-	// ------------ INSERT CODE HERE! -----------------------------------
-
-	return videos;
+  return videos;
 }
 
 //7. Implement myFilter()
 Array.prototype.myFilter = function(predicateFunction) {
-	var results = [];
-	this.forEach(function(itemInArray) {
-		// ------------ INSERT CODE HERE! ----------------------------
-		// Apply the predicateFunction to each item in the array.
-		// If the result is truthy, add the item to the results array.
-
-
-	});
-
-	return results;
+  var results = [];
+  this.forEach(function(itemInArray) {
+    // ------------ INSERT CODE HERE! ----------------------------
+    // Apply the predicateFunction to each item in the array.
+    // If the result is truthy, add the item to the results array.
+    if (predicateFunction(itemInArray)) {
+      results.push(itemInArray);
+    };
+  });
+  return results;
 };
 
 //Query Data by Chaining Method Calls
 
 //8. Chain filter and map to collect the ids of videos that have a rating of 5.0
-function goodMovieId(arrayOfObjects){
-  return arrayOfObjects// finish this expression!
-
+function goodMovieId(arrayOfObjects) {
+  var caca = function(item) {
+    if (item.rating == 5) {
+      return true;
+    }
+  };
+  var cucu = function(item) {
+    return ({
+      id: item.id,
+    });
+  }
+  return arrayOfObjects.myFilter(caca).myMap(cucu);
+  // finish this expression!
 }
-
 //Reducing Arrays
 //Let's say we need to find the largest integer in an array.
 //We can't use a filter() operation, because it only examines one item at a time.
@@ -99,54 +122,53 @@ function goodMovieId(arrayOfObjects){
 function longestMovie(arrayOfObjects) {
 
   //something is missing
+  var maxLength = 0;
+  var currentLongest = 0;
+  var longestVideo;
+  arrayOfObjects.forEach(function(video) {
+    currentLongest = video.minutes;
+    if (currentLongest > maxLength) {
+      longestVideo = video;
+      maxLength = currentLongest;
+    }
+  });
 
-	arrayOfObjects.forEach(function(video) {
-		currentLongest = video.minutes;
-		if (currentLongest > maxLength) {
-			longestVideo = video;
-			maxLength = currentLongest;
-		}
-	});
-
-	return longestVideo.title;
+  return longestVideo.title;
 }
 
 //10. Implement reduce()
 Array.prototype.myReduce = function(combiner, initialValue) {
-	var counter,
-		  accumulatedValue;
+  var counter,
+    accumulatedValue;
 
-	// If the array is empty, do nothing
-	if (this.length === 0) {
-		return initialValue;
-	}
-	else {
-		// If the user didn't pass an initial value, use the first item.
-		if (arguments.length === 1) {
-			counter = 1;
-			accumulatedValue = this[0];
-		}
-		else if (arguments.length >= 2) {
-			counter = 0;
-			accumulatedValue = initialValue;
-		}
-		else {
-			throw "Invalid arguments.";
-		}
+  // If the array is empty, do nothing
+  if (this.length === 0) {
+    return initialValue;
+  } else {
+    // If the user didn't pass an initial value, use the first item.
+    if (arguments.length === 1) {
+      counter = 1;
+      accumulatedValue = this[0];
+    } else if (arguments.length >= 2) {
+      counter = 0;
+      accumulatedValue = initialValue;
+    } else {
+      throw "Invalid arguments.";
+    }
 
-		// Loop through the array, feeding the current value and the result of
-		// the previous computation back into the combiner function until
-		// we've exhausted the entire array.
-		while(counter < this.length) {
-      // ------------ INSERT CODE HERE! ----------------------------
-		}
-		return accumulatedValue;
-	}
+    // Loop through the array, feeding the current value and the result of
+    // the previous computation back into the combiner function until
+    // we've exhausted the entire array.
+    while (counter < this.length) {
+      counter++; // ------------ INSERT CODE HERE! ----------------------------
+    }
+    return accumulatedValue;
+  }
 };
 
 //11. Use myReduce function to isolate the largest value in an array of ratings.
 function highestRating(ratings) {
-  	return ratings.myReduce(function(acc, curr) { // Complete this expression
+  return ratings.myReduce(function(acc, curr) { // Complete this expression
 
   });
 }
